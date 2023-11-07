4ee00728a0339b9bc48684925c345a8ac32943e2
Carlos Aviles Nuñez <122574326+cavilesnunez@users.noreply.github.com>
2023-11-07 15:39:04 -0600

    first commit

--- src/utils/bcrypt.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcrypt'
-
-//Encriptar contraseña
-export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(parseInt(process.env.SALT)))
-
-//Validar contraseña encriptada
-export const validatePassword = (passwordSend, passwordBDD) => bcrypt.compareSync(passwordSend, passwordBDD)
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,hbs,handlebars}"], // Nota la adición de .hbs aquí
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
